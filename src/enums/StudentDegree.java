64bf37b8fbafb2d85e551916cb4bcb1eb129f2ea
Saken Meirzhan <meirzhansaken096@gmail.com>
2026-04-29 20:00:59 +0500

    adding enums

---
+++ src/enums/StudentDegree.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum StudentDegree {
+    BACHELOR,
+    MASTER,
+    PHD
+}
--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,40 @@
 package model;
 
+import enums.Language;
+
 public class User {
+    private String id;
+    private String fullName;
+    private String username;
+    private String password;
+    private Language language;
+
+    public User(String id, String fullName,String username, String password, Language language ){
+        this.id = id;
+        this.fullName = fullName;
+        this.username = username;
+        this.password = password;
+        this.language = language;
+    }
+
+    @Override
+    public String toString() {
+        return "User`s full name: " + fullName + " nickname: " + username + " System Language: " + language;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
 }
